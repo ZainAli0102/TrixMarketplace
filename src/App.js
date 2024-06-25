d8a15fce7c4f1200f57c8ba90f174931d3aef142
tabitabasharat <tabitab632@gmail.com>
2024-06-25 17:27:28 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import './App.css';
-import Header from './Components/Header/Header';
-import Main from './Components/main/Main';
-import NFTsFeatures from './Components/NFTsFeatures/NFTsFeatures';
-import CreateNFTs from './Components/CreateNFTs/CreateNFTs';
-import DiscoverNFTs from './Components/DiscoverNFTs/DiscoverNFTs';
-
+import "./App.css";
+import Main from "./Components/main/Main";
+import NFTsFeatures from "./Components/NFTsFeatures/NFTsFeatures";
+import CreateNFTs from "./Components/CreateNFTs/CreateNFTs";
+import DiscoverNFTs from "./Components/DiscoverNFTs/DiscoverNFTs";
 
 function App() {
   return (
     <div>
-      {/* <Header/> */}
-       <Main/>
-      <NFTsFeatures/>
-     <CreateNFTs/> 
-      <DiscoverNFTs/> 
+      <Main />
+      <NFTsFeatures />
+      <CreateNFTs />
+      <DiscoverNFTs />
     </div>
   );
 }
